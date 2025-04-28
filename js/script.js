ddbd12d09b034c610e94b4d453bac43c30b414ba
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2025-04-28 20:01:57 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.fade-section');
+
+  window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      
+      // Kalau bagian masuk ke layar (misal 100px sebelum viewport bawah)
+      if (rect.top < window.innerHeight - 100 && rect.bottom > 100) {
+        section.classList.add('show');
+        section.classList.remove('hide');
+      } 
+      // Kalau bagian sudah keluar layar
+      else {
+        section.classList.remove('show');
+        section.classList.add('hide');
+      }
+    });
+  });
+  
