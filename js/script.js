41f5d8c4fb4b5793b9b0c4dc285bb85232c70c48
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2025-04-29 10:03:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 const sections = document.querySelectorAll('.fade-section');
 
-  window.addEventListener('scroll', () => {
-    sections.forEach(section => {
-      const rect = section.getBoundingClientRect();
-      
-      // Kalau bagian masuk ke layar (misal 100px sebelum viewport bawah)
-      if (rect.top < window.innerHeight - 100 && rect.bottom > 100) {
-        section.classList.add('show');
-        section.classList.remove('hide');
-      } 
-      // Kalau bagian sudah keluar layar
-      else {
-        section.classList.remove('show');
-        section.classList.add('hide');
-      }
-    });
+window.addEventListener('scroll', () => {
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+
+    // Kalau bagian masuk ke layar (misal 100px sebelum viewport bawah)
+    if (rect.top < window.innerHeight - 100 && rect.bottom > 100) {
+      section.classList.add('show');
+      section.classList.remove('hide');
+    }
+    // Kalau bagian sudah keluar layar
+    else {
+      section.classList.remove('show');
+      section.classList.add('hide');
+    }
   });
-  
+});
+
+function toggleMenu() {
+  const menuList = document.getElementById('menu-list');
+  const menuExtra = document.getElementById('menu-extra');
+
+  menuList.classList.toggle('show');
+  menuExtra.classList.toggle('show');
+}
